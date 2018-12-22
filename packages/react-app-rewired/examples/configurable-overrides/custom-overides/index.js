20a6dc4966f8abaf258ecb2801a765335fa9eaae
zhujianchen <zhujianchen@bbktel.com>
2018-12-22 20:26:43 +0800

    remove console

--- packages/react-app-rewired/examples/configurable-overrides/custom-overides/index.js
+++ packages/react-app-rewired/examples/configurable-overrides/custom-overides/index.js
@@ -2,8 +2,6 @@ const { injectBabelPlugin } = require('react-app-rewired-comp');
 
 /* config-overrides.js */
 module.exports = function override(config, env) {
-  console.trace();
-  console.log(config)
   config = injectBabelPlugin(['@babel/plugin-proposal-decorators', {
     legacy: true
   }], config);
